0c1ffd1c0fdc2ac95eafd32102975f43c21eb083
ramonruiz <jruiz@lightia.mx>
2023-10-23 10:31:11 -0600

    Numbers

---
+++ 05_numbers.py
@@ -0,0 +1,35 @@
+# Integer
+lives = 3
+print(type(lives))
+age = 22
+buget = 100
+
+# Float
+temperature = 12.12
+print(type(temperature))
+
+# Operaciones
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print(lives)
+
+lives -= 1
+print(lives)
+
+lives += 5
+print(lives)
+
+number = 45000000000000000.1
+print(number)
+
+numberB = 0.0000000000001
+print(numberB)
+
+junuary = 30000
+february = 30000
+march = 30000
+
+quarter = junuary + february + march
+print(quarter)

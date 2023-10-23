527293c6fe01605d5a77e214092f2d6fab143af1
ramonruiz <jruiz@lightia.mx>
2023-10-23 10:16:14 -0600

    Tipos de datos

---
+++ 03_types.py
@@ -0,0 +1,19 @@
+# string
+my_name = 'Ramón'
+print(my_name)
+print(type(my_name))
+
+# integer
+my_age = 22
+print(my_age)
+print(type(my_age))
+
+# boolean
+is_old = True
+print(is_old)
+print(type(is_old))
+
+# Input
+my_name = input('Ingresa tu edad: ')
+print('Usando edad', my_name)
+print(type(my_name))

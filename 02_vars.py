e2d3c837c55dafd20fa26753e148c6d29e5579f6
ramonruiz <jruiz@lightia.mx>
2023-10-23 10:06:28 -0600

    Variables

---
+++ 02_vars.py
@@ -0,0 +1,15 @@
+# Esto es una variable
+my_name = 'Ramón'
+print(my_name)
+
+# Esto es una variable numérica
+my_age = 22
+print(my_age)
+
+# Esto es una variable reasignada
+my_name = 'Juan'
+print('Aquí cambio', my_name)
+
+# Input
+my_name = input('Ingresa tu nombre: ')
+print('Usando input', my_name)

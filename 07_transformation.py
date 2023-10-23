93e32da1025973278be9a16ac42b69816845bdcb
ramonruiz <jruiz@lightia.mx>
2023-10-23 10:46:42 -0600

    Transformación de tipos

---
+++ 07_transformation.py
@@ -0,0 +1,22 @@
+# Transformación
+name = 'Ramón'
+print(type(name))
+name = 22
+print(type(name))
+name = True
+print(type(name))
+
+print('Ramón' + ' Ruiz')
+print(22 + 22)
+print('Ramón' + " 22")
+
+age = 22
+print('Mi edad es ' + str(age))
+print(f"Mi edad es {age}")
+
+age = input('Ingresa tu edad: ')
+print(type(age))
+age = int(age)
+age = age + 10
+print(f"Mi edad en 10 años será {age}")
+

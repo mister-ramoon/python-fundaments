310813002f0768af13a160b20f3ed4c315f3f223
ramonruiz <jruiz@lightia.mx>
2023-10-23 10:00:28 -0600

    Tu primer programa

---
+++ main.py
@@ -0,0 +1,20 @@
+print("Hola, esto es Python")
+print("Hola, Soy un programador de Python")
+
+# Operaciones matemáticas
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario
+""""
+    Varias
+    Lineas
+"""
+
+'''
+    Varias
+    Lineas
+'''

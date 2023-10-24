be7f1d0c5f80389fedc67e6ac051d20dbf1949ff
mr_ramon <ramonruizrc@gmail.com>
2023-10-24 06:55:11 -0600

    Operadores aritméticos

---
+++ 08_arith.py
@@ -0,0 +1,11 @@
+print(10 + 10) # 20
+print(10 - 10) # 0
+print(10 * 10) # 100
+print(10 / 2) # 5.0
+print(10 % 2) # 0
+print(10 // 3) # 3
+print(2 ** 3) # 8
+print(2 ** 3 + 3 - 7 / 1 // 4) # 9.0
+
+print('Hola' + 'Mundo')
+print('Hola' * 3)

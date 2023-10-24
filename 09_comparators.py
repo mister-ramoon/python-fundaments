51f5afeb876a857a400a5a18e09459a8bec6466d
ramonruiz <jruiz@lightia.mx>
2023-10-24 09:18:41 -0600

    Operadores de comparación

---
+++ 09_comparators.py
@@ -0,0 +1,22 @@
+# >
+print (7 > 3) # True
+print (7 < 3) # False
+print (7 >= 3) # True
+
+# <
+print(5 < 6) # True
+print(5 < 5) # False
+print(5 < 4) # False
+print(5 <= 5) # True
+
+# ==
+print(5 == 5) # True
+print(5 == 4) # False
+print(5 == '5') # False
+print('Apple' == "Apple") # True
+print('Apple' == "apple") # False
+
+# !=
+print(5 != 5) # False
+print(5 != 4) # True
+print(5 != '5') # True

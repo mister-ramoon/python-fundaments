2f8eca84acfa215ab287de6ac3875b50e197f4aa
ramonruiz <jruiz@lightia.mx>
2023-10-25 09:34:53 -0600

    Indexing y slicing

---
+++ indexing.py
@@ -0,0 +1,16 @@
+text = 'Ella sabe Python'
+print(text[0])
+print(text[1])
+
+size = len(text)
+print(text[size - 1])
+print(text[-1])
+
+# slicing
+print(text[0:4])
+print(text[5:9])
+print(text[5:])
+print(text[:9])
+print(text[5:-1])
+print(text[10:16:1])
+print(text[::2])

7066347fffa196883737010bf53aeb2c799980a0
ramonruiz <jruiz@lightia.mx>
2023-10-25 09:43:05 -0600

    Listas

---
+++ 16_list.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4]
+print(numbers)
+print(type(numbers))
+
+tasks = ['Sacar la basura', 'Barrer la entrada', 'Pasear al perro']
+print(tasks)
+
+types = [1, 'Hola', 2.5, True, [1, 2, 3]]
+print(types)
+
+print(tasks[0])
+
+tasks[0] = 'Lavar los trastes'
+print(tasks)
+
+print(numbers[:3])
+print(True in types)
